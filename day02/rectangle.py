436a415ea461d33e1b36902810703de360e891c7
Shirikara <shirikara@gmail.com>
2024-11-13 11:02:44 +0200

    Add rectangle.py to repository from the VS code Git extention

---
+++ day02/rectangle.py
@@ -0,0 +1,15 @@
+# ----- This program prints out the area and the circumference of the rectangle -----#
+
+# Define width
+rec_width = 8
+
+# Define length
+rec_length = 12
+
+# Area of the rectangle
+rec_area = rec_width * rec_length
+print("The area of the rectangle is: ", rec_area)
+
+# Circumference of the rectangle
+rec_circumference = 2*rec_width + 2*rec_length
+print("The circumference of the rectangle is: ", rec_circumference)
