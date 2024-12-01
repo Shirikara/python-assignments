ff4e716b1aa8bf324d1026438ab2fae934d850b5
Shirikara <shirikara@gmail.com>
2024-12-01 11:40:24 +0200

    Improve comments and user messages

--- day04/Day04_Guess_Number_Game.py
+++ day04/Day04_Guess_Number_Game.py
@@ -6,16 +6,18 @@ import numpy as np
 # Define the game inside a function
 def guess_number_game():
     print("Welcome to the telepathy game!")
-    total_num_games = 0 # for printing the user at the end
-    play_again = True
+    # Initializing Game Variables
+    total_num_games = 0 #keeps track of how many games the user has played
+    play_again = True #controls the main game. It starts as True so the user can play at least one game
 
     while play_again:
         # Generate a random number for the game
         comp_int_1to20 = np.random.randint(1, 21) #random int between 1 and 20 
-        total_num_games += 1
+        total_num_games += 1 #counts if new game is starting
         num_try = 0 #How many times it takes the user to guess correctly within one round
-        keep_playing = True
+        keep_playing = True #This loop keeps running as long as the user wants to play (keep_playing = True)
 
+        #Game instructions and special commands for the user
         print("\nI am thinking of a number between 1 and 20. Can you guess what it is?")
         print("Enter 'x' to exit the program, 'n' to start a new game, or 's' to show the hidden number.")
 
@@ -36,7 +38,7 @@ def guess_number_game():
                     num_try += 1
 
                     if guess_by_user == comp_int_1to20:
-                        print(f"Yay! You guessed correctly in {num_try} tries. You won the game!")
+                        print(f"Yay! You guessed correctly in {num_try} tries. You read my thoughta and won the game!")
                         keep_playing = False
                     elif guess_by_user < 0:
                         print("Only positive values are accepted. Please try again")
